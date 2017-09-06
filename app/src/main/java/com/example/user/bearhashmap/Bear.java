127d706821a802571fe0301156cc5dc02963fac9
Benjamin Conway <benrconway84@gmail.com>
2017-09-06 09:36:49 +0100

    Add homework revisions

--- app/src/main/java/com/example/user/bearhashmap/Bear.java
+++ app/src/main/java/com/example/user/bearhashmap/Bear.java
@@ -6,7 +6,6 @@ package com.example.user.bearhashmap;
 import java.util.*;
 
 public class Bear {
-    private String name;
     private HashMap<String, Integer> journal;
     private ArrayList<Salmon> belly;
 
@@ -14,42 +13,33 @@ public class Bear {
     public Bear(){
         this.belly = new ArrayList<>();
         this.journal = new HashMap<>();
-        journal.put("monday", 0);
-        journal.put("tuesday", 0);
-        journal.put("wednesday", 0);
-        journal.put("thursday", 0);
-        journal.put("friday", 0);
-        journal.put("saturday", 0);
-        journal.put("sunday", 0);
-    }
-
-    public String getName() {
-        return name;
+        this.newWeek();
     }
 
     public int foodCount(){
         return belly.size();
     }
 
-
-    public HashMap<String, Integer> getJournal() {
-        return journal;
-    }
-
     public void eat(Salmon salmon, String dayEaten){
         belly.add(salmon);
-        addEntry(dayEaten);
+        addToEntry(dayEaten);
+
+        // if (!journal.containsKey(day)){
+        // int newCount = journal.get(day) + 1;
+        // journal.put(day, newCount);
+        // }
     }
 
     public int checkJournal(String weekday){
         return journal.get(weekday);
     }
 
-    public void addEntry(String dayOfWeek){
+    public void addToEntry(String dayOfWeek){
         journal.put(dayOfWeek, journal.get(dayOfWeek) +1);
     }
 
     public void newWeek(){
+        journal.clear(); //To be sure that it will clear and restart//
         journal.put("monday", 0);
         journal.put("tuesday", 0);
         journal.put("wednesday", 0);
@@ -58,5 +48,10 @@ public class Bear {
         journal.put("saturday", 0);
         journal.put("sunday", 0);
     }
-
+// Iterating over the hashmap.
+//    public void resetJournal(){
+//        for (String day: journal.keySet()){
+//            journal.put(day, 0);
+//        }
+//    }
 }
